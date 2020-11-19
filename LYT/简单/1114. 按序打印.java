57e2eb44e8d610f74e165c23299d4e0ce2d656ee
绊清风 <7950884+trip_up_the_breeze@user.noreply.gitee.com>
2020-11-19 17:56:25 +0800

    add LYT/简单/1114. 按序打印.java.

---
+++ LYT/简单/1114. 按序打印.java
@@ -0,0 +1,39 @@
+// 我们提供了一个类：
+
+// public class Foo {
+//   public void first() { print("first"); }
+//   public void second() { print("second"); }
+//   public void third() { print("third"); }
+// }
+// 三个不同的线程将会共用一个 Foo 实例。
+
+// 线程 A 将会调用 first() 方法
+// 线程 B 将会调用 second() 方法
+// 线程 C 将会调用 third() 方法
+// 请设计修改程序，以确保 second() 方法在 first() 方法之后被执行，third() 方法在 second() 方法之后被执行。
+
+class Foo {
+
+  private AtomicInteger firstJobDone = new AtomicInteger(0);
+  private AtomicInteger secondJobDone = new AtomicInteger(0);
+
+  public Foo() {}
+
+  public void first(Runnable printFirst) throws InterruptedException {
+    printFirst.run();
+    firstJobDone.incrementAndGet();
+  }
+
+  public void second(Runnable printSecond) throws InterruptedException {
+    while (firstJobDone.get() != 1) {
+    }
+    printSecond.run();
+    secondJobDone.incrementAndGet();
+  }
+
+  public void third(Runnable printThird) throws InterruptedException {
+    while (secondJobDone.get() != 1) {
+    }
+    printThird.run();
+  }
+}
