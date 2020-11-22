23aa91d1c3bb5bc5946513aed37706e48a93ad0c
绊清风 <7950884+trip_up_the_breeze@user.noreply.gitee.com>
2020-11-22 23:09:41 +0800

    add LYT/简单/242. 有效的字母异位词.java.

---
+++ LYT/简单/242. 有效的字母异位词.java
@@ -0,0 +1,37 @@
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+//输入: s = "anagram", t = "nagaram"
+//输出: true
+
+//方法一：排序
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
+
+//方法二：字母表
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
