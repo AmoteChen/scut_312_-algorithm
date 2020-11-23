d11be63aa1119e51a1b1a849d39daf0c9b080f57
GeorgeLee_10 <8255340+georgeleenihao@user.noreply.gitee.com>
2020-11-23 23:20:53 +0800

    rename LZG/简单/SortStackWithAnotherStack.java to LZG/简单/SortStackByStack.java. stack元素逐一放到help中，若当前元素大于help栈顶元素，需要将help放回stack，直到找到cur该在的位置。 最后将help逐一压入stack

---
+++ LZG/简单/SortStackByStack.java
@@ -0,0 +1,19 @@
+#一个栈中元素为整型，现在将这个栈从顶到底按从大到小的顺序排序，只允许申请一个栈，不能申请额外的数据结构
+public static void sortStackByStack(Stack<Integer> stack)
+    {
+        Stack<Integer> help=new Stack<>();
+        while (!stack.isEmpty())
+        {
+            int cur = stack.pop();
+            if (!help.isEmpty()) {
+                while (cur > help.peek()&&!help.isEmpty()) {
+                    stack.push(help.pop());
+                }
+            }
+            help.push(cur);
+        }
+        while (!help.isEmpty())
+        {
+            stack.push(help.pop());//排序结束，放回原栈
+        }
+    }
