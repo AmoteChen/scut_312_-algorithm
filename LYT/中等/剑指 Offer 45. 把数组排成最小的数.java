ecde968801d36bd8d541559c0f3ad1d5a6103b46
绊清风 <7950884+trip_up_the_breeze@user.noreply.gitee.com>
2020-11-23 23:13:27 +0800

    add LYT/中等/剑指 Offer 45. 把数组排成最小的数.java.

---
+++ LYT/中等/剑指 Offer 45. 把数组排成最小的数.java
@@ -0,0 +1,49 @@
+// 输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+// 输入: [3,30,34,5,9]
+// 输出: "3033459"
+// 若拼接字符串 x + y > y + x ，则 m > n 
+// 反之，若 x + y < y + x ，则 n < m
+// 根据以上规则，套用任何排序方法对 nums 执行排序即可
+
+// 方法一：内置排序算法
+class Solution {
+    public String minNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++) 
+            strs[i] = String.valueOf(nums[i]);
+        Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
+    }
+}
+//方法二：快排
+class Solution {
+    public String minNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            strs[i] = String.valueOf(nums[i]);
+        fastSort(strs, 0, strs.length - 1);
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
+    }
+    void fastSort(String[] strs, int l, int r) {
+        if(l >= r) return;
+        int i = l, j = r;
+        String tmp = strs[i];
+        while(i < j) {
+            while((strs[j] + strs[l]).compareTo(strs[l] + strs[j]) >= 0 && i < j) j--;
+            while((strs[i] + strs[l]).compareTo(strs[l] + strs[i]) <= 0 && i < j) i++;
+            tmp = strs[i];
+            strs[i] = strs[j];
+            strs[j] = tmp;
+        }
+        strs[i] = strs[l];
+        strs[l] = tmp;
+        fastSort(strs, l, i - 1);
+        fastSort(strs, i + 1, r);
+    }
+}
