5c04ffcd9bd76674f42bd5545fc67c9202e065f3
AmoteChen <948167893@qq.com>
2020-11-25 10:42:51 +0800

    Java_race

---
+++ CSY/learning_Java/Race.java
@@ -0,0 +1,43 @@
+package com.csy.www;
+
+public class Race implements Runnable{
+
+    private static String winner;
+    @Override
+    public void run() {
+        for (int i = 0; i <= 100; i++) {
+            boolean flag = gameOver(i);
+            if(Thread.currentThread().getName().equals("兔子")&& i%10==0){
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(flag){
+                break;
+            }
+            System.out.println(Thread.currentThread().getName()+"跑了"+i+"步");
+        }
+    }
+
+    private boolean gameOver(int steps){
+        if (winner != null){
+            return true;
+        }
+        if(steps>=100){
+            winner = Thread.currentThread().getName();
+            System.out.println("Winner is "+ winner);
+            return true;
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        Race race = new Race();
+        new Thread(race,"兔子").start();
+        new Thread(race,"乌龟").start();
+
+    }
+}
