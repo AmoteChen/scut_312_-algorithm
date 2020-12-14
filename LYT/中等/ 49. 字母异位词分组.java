cd69571c77e377504b5c956df0d79114d1c1e983
绊清风 <7950884+trip_up_the_breeze@user.noreply.gitee.com>
2020-12-14 23:48:08 +0800

    add LYT/中等/ 49. 字母异位词分组.java.

---
+++ LYT/中等/ 49. 字母异位词分组.java
@@ -0,0 +1,26 @@
+// 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+
+// 示例:
+
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+// 输出:
+// [
+//   ["ate","eat","tea"],
+//   ["nat","tan"],
+//   ["bat"]
+// ]
+//排序法，对每个字符串进行排序，排序后的string作为key！
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());//存在这个key就用以前的数组，否则重新建立
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
